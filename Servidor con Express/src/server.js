7d3922c41477ef7ea39a3ccdb8d6df611aef6ce9
hgerez32 <gerez_hernan@live.com>
2024-05-07 16:55:25 -0300

    3er entrega

---
+++ Servidor con Express/src/server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import ProductManager from "./manager/product.manager.js";
+
+const productManager = new ProductManager("./products.json");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await productManager.getProduct();
+
+    const { limit } = req.query
+    const productsLimit = products.slice(0,limit).map(p);
+
+    products.prototype.last = function(limit){
+        return this.slice(limit)
+      };
+      
+      console.log( arr.last )
+
+
+    
+    console.log(productsLimit);
+
+    // res.status(201).json(productsLimit)
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    // console.log(req.body);
+    const products = await productManager.addProduct(req.body);
+    res.status(201).json(products);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const { idProdpiduct } = req.params;
+    const product = await productManager.getUserById(pid);
+    if (!product) res.status(404).json({ msg: "User not found" });
+    else res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.put("/products/:pid", async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const ProdUpd = await productManager.updateProduct(req.body, pid);
+    if (!ProdUpd) res.status(404).json({ msg: "Error updating user" });
+    res.status(200).json(ProdUpd);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.delete("/products/:pid", async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const delProd = await productManager.deleteUser(pid);
+    if(!delProd) res.status(404).json({ msg: "Error delete user" });
+    else res.status(200).json({msg : `User id: ${pid} deleted successfully`})
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => console.log(`Server ok on port ${PORT}`));
